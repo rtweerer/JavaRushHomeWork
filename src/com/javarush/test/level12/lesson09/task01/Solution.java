ebbc2b2789ad695eed7b66979ba917d77a05b09d
Bogdkad <bohdan.kaidanovych@gmail.com>
2013-07-31 12:34:10 +0300

    new

---
+++ src/com/javarush/test/level12/lesson09/task01/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level12.lesson09.task01;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public interface Fly
+    {
+        public void fly();
+
+        public void swim();
+    }
+}
